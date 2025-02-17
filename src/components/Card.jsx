77a66a8de0ca8e235b65fcd653bbf49bcc3e6ecf
Gabriel Cordeiro <120519526+GabrielFRCordeiro@users.noreply.github.com>
2025-02-17 14:33:22 -0300

    feat(Cards): estilizando e colocando scroll

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,21 +6,25 @@ export default function Card({titulo, nota, preco}) {
   return (
     <>
         <div className="img_produto"></div>
-        <div className="d-flex px-2 pt-3 info_produto">
-            <div className="col-6 d-flex flex-column justify-content-between">
+        <div className="px-2 pt-3 info_produto">
+            <div className="row">
                 <h5>{titulo}</h5>
-                <div>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-half"></i>
-                    <i class="bi bi-star"></i>
-                    <i class="bi bi-star"></i>
-                </div>
-                <p className='m-0'>R$ {preco}</p>
             </div>
-            <div className="col-6 align-self-end d-flex justify-content-around">
-                <button className='card_btn btn_carrinho d-flex align-items-center py-1'><img src={ShoppingCart} alt="" /></button>
-                <button className='card_btn btn_favorito d-flex align-items-center py-1'><img src={Heart} alt="" /></button>
+            <div className="row">
+                <div className="col-7 d-flex flex-column justify-content-between">
+                    <div>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-fill"></i>
+                        <i class="bi bi-star-half"></i>
+                        <i class="bi bi-star"></i>
+                        <i class="bi bi-star"></i>
+                    </div>
+                    <p className='m-0'>R$ {preco}</p>
+                </div>
+                <div className="col-3 px-0 align-self-end d-flex justify-content-around">
+                    <button className='card_btn btn_carrinho d-flex align-items-center py-1 me-2'><img src={ShoppingCart} alt="" /></button>
+                    <button className='card_btn btn_favorito d-flex align-items-center py-1'><img src={Heart} alt="" /></button>
+                </div>
             </div>
         </div>
     </>
