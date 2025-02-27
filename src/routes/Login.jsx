94c007ac7bef61df0ec9176dda40f1a946528be1
ManuFlorenca! <manuelaflorenca22@gmail.com>
2025-02-27 11:16:19 -0300

    feat(tela login): começando a estruturar a tela

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,6 +1,19 @@
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+      <div className="img-container">
+        <img src="" alt="" />
+      </div>
+      <div className="login-container">
+        <h1>Bem vindo!</h1>
+        <form>
+          <input type="text" name="name" id="t_field_name" />
+          <input type="password" id="t_field_password" />
+          <a href="">Esqueci minha senha</a>
+          
+        </form>
+      </div>
+    </div>
   )
 }
 
